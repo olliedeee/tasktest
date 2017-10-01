d92e13622d837d03b9e7ad8d9d5e0ecff3127c43
Ollie <oliverbucks@gmail.com>
2017-10-01 17:11:45 +0000

    added goals and tasks creation, deletion

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :tasks
-  resources :goals
+  resources :goals do 
+    resources :tasks
+  end
+  
   resources :statics
-  root 'statics#index'
+  root 'goals#index'
   devise_for :users
   resources :users, except: [:new]
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
